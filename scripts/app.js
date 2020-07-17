78b7ab61917d2bf9b27f8bc4eafb0a3bb5212f79
brendino500 <brenda.ty@live.com>
2020-07-17 18:13:35 +0100

    worked basic scoring and colours of tetriminos

--- scripts/app.js
+++ scripts/app.js
@@ -51,12 +51,32 @@ function init() {
 
   // * SCORING LOGIC
 
-  // ?? Level 0-1   1 line = 100  2 lines = 400   3 lines = 900   4 lines = 2000
-  // ?? Level 2-3   1 line = 200  2 lines = 800   3 lines = 1800  4 lines = 4000
-  // ?? Level 4-5   1 line = 300  2 lines = 1200  3 lines = 2700  4 lines = 6000
-  // ?? Level 6-7   1 line = 400  2 lines = 1600  3 lines = 3600  4 lines = 8000
-  // ?? Level 8+    1 line = 500  2 lines = 2000  3 lines = 4500  4 lines = 10000
+  // ?? Level 0-1   1 line = 100  2 lines = 400   3 lines = 900   4 lines = 2000    10 cleared lines
+  // ?? Level 2-3   1 line = 200  2 lines = 800   3 lines = 1800  4 lines = 4000    20 cleared lines
+  // ?? Level 4-5   1 line = 300  2 lines = 1200  3 lines = 2700  4 lines = 6000    30 cleared lines
+  // ?? Level 6-7   1 line = 400  2 lines = 1600  3 lines = 3600  4 lines = 8000    40 cleared lines
+  // ?? Level 8+    1 line = 500  2 lines = 2000  3 lines = 4500  4 lines = 10000   50 cleared lines
+  // ?? Clear 5 lines to get onto next level
 
+  let linesCleared = 0
+  let currentLevel = 0
+
+  if (linesCleared <= 10) {
+    currentLevel++
+  } else if (linesCleared > 10 || linesCleared <= 20) {
+    currentLevel++
+  } else if (linesCleared > 10 || linesCleared <= 20) {
+    currentLevel++
+  } else if (linesCleared > 20 || linesCleared <= 30) {
+    currentLevel++
+  } else if (linesCleared > 30 || linesCleared <= 40) {
+    currentLevel++
+  } else (linesCleared > 40)
+  return currentLevel++
+
+
+
+}
   
 
 
@@ -65,6 +85,21 @@ function init() {
 
 
 
-}
+
 
 window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+// !! RECURSION 
+
+function countDownFrom(num) {
+  console.log(num)
+  if (num === 0) {
+    return 
+  }
+  countDownFrom(num - 1)
+}
+
+countDownFrom(10)
